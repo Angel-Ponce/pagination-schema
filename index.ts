631fdafe0fb9787850d30a8d9007541d5f5ccc06
Angel Ponce <acdpf000@gmail.com>
2023-08-16 08:31:55 -0600

    paginate function 📄

---
+++ index.ts
@@ -0,0 +1,48 @@
+export interface PaginationConfig {
+  total: number;
+  perPage: number;
+  currentPage: number;
+}
+
+const paginate = (config: PaginationConfig) => {
+  const pages = new Array(Math.ceil(config.total / config.perPage))
+    .fill(0)
+    .map((_o, i) => i + 1);
+
+  if (pages.length <= 6) {
+    return pages;
+  }
+
+  const start = [pages[0], pages[1]];
+  const final = [pages[pages.length - 2], pages[pages.length - 1]];
+
+  const startNearest = start[1] == config.currentPage - 1;
+  const finalNearest = final[0] == config.currentPage + 1;
+
+  if (
+    start.includes(config.currentPage) ||
+    final.includes(config.currentPage)
+  ) {
+    return [...start, start[1] + 1, 0, final[0] - 1, ...final];
+  }
+
+  if (startNearest) {
+    return [...start, start[1] + 1, start[1] + 2, 0, final[1]];
+  }
+
+  if (finalNearest) {
+    return [start[0], 0, final[0] - 2, final[0] - 1, ...final];
+  }
+
+  return [
+    start[0],
+    0,
+    config.currentPage - 1,
+    config.currentPage,
+    config.currentPage + 1,
+    0,
+    final[1],
+  ];
+};
+
+export { paginate };
